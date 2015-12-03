46f28c7634124b5b8b846475ee39c43eac5ff9da
Norrbrinken <pierrenorrbrink@gmail.com>
2015-12-03 23:27:51 +0100

    starting

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE >
+<html>
+	<head>
+		<meta http-equiv="Content-Type"	content="text/html" charset="UTF-8">
+		<link href="css/css.css" rel="stylesheet" type="text/css">
+		<script src="js/js.js"></script>
+	</head>
+	<body>
+		<div class="pageContainer">
+			<h1>DHS16</h1>
+			<nav class="mainNav">
+				<ul>
+					<li>Checkin</li>
+					<li>Checkout</li>
+				</ul>
+			</nav>
+			<form action="#" method="POST" id="checkin">
+				<label for="name">Name:</label><input type="text" id="name" />
+				<label for="problem">Problem:</label><textarea id="problem"></textarea>
+				<input type="submit" />
+			</form>
+		</div>
+	</body>
+</html>
