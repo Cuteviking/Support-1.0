ebc43fadd99187756ee7d01978094b2273362161
Norrbrinken <pierrenorrbrink@gmail.com>
2015-12-04 16:21:12 +0100

    poop

--- checkout.php
+++ checkout.php
@@ -16,13 +16,18 @@
 					</ul>
 				</nav>
 			</header>
-			<form action="#" method="POST" id="checkin">
-				<label for="name">Name:</label><input type="text" id="name" />
-				<label for="plats">Plats:</label><input type="text" id="plats" />
+			<form action="#" method="POST" id="checkout">
+				<label for="id">ID:</div><input type="text" id="id" />
+				<input type="submit" />
+			</form>
+			<!-- hämta data -->
+			<form action="#" method="POST" id="">
+				<div>Name:</div>
+				<div>Plats:</div>
 				<span>ID</span>
-				<label for="problem">Problem:</label><textarea id="problem"></textarea>
-				<label for="guess">Guess:</label><textarea id="guess"></textarea>
-				<label for="parts">Delar:</label><textarea id="parts" placeholder="Eventuella delar"></textarea>
+				<div>Problem:</div>
+				<div>Guess:</div>
+				<div>Delar:</div>
 				<input type="submit" />
 			</form>
 		</div>
