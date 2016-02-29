ab9d3b2e2302bf4278c35d6699357060b42d0b70
brunnen96 <olajohansson96@gmail.com>
2016-02-29 19:56:54 +0100

    småsaker bara

--- index.php
+++ index.php
@@ -23,10 +23,10 @@
 						<label for="name">Nick:</label><input type="text" id="name" name="name" />
 					</section>
 					<section>
-						<label for="social">Social security number:</label><input name="social" type="text" id="social" placeholder="YYYY-MM-DD-XXXX"/>
+						<label for="social">Social security number:</label><input name="social" type="text" id="social" placeholder="YYYYMMDDXXXX (möst be 12 digits)" maxlength="12"/>
 					</section>
 					<section>
-						<label for="seat">Seat:</label><input name="seat" type="text" id="seat" />
+						<label for="seat">Seat:</label><input name="seat" type="text" placeholder="e.g. D24:05" id="seat" />
 					</section>
 					<section>
                     	<label for="phone">Phone nr:</label><input name="phone" type="text" id="phone" />
@@ -34,13 +34,13 @@
 				</div>
 				<div class="problem">
 					<section>
-						<label for="problem">Problem:</label><input name="problem" id="problem" type="text" />
+						<label for="problem">Problem title:</label><input name="problem" id="problem" placeholder="e.g. no picture on the screen" type="text" />
 					</section>
 					<section>
 						<label for="problemDesc">Problem Description:</label><textarea name="problemDesc" id="problemDesc" ></textarea>
 					</section>
 					<section>
-						<label for="parts">Parts:</label><textarea id="parts" name="parts" ></textarea>
+						<label for="parts">Associated parts:</label><textarea id="parts" name="parts" placeholder="e.g. MSI GeForce GTX 980 Ti Gaming 6GB"></textarea>
 					</section>
 				</div>
 				<footer>
